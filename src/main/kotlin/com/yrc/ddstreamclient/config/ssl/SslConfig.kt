2b8f182c83865ad8121fc91926f7626fcb5b4187
yrc <958480638@qq.com>
2022-02-05 18:32:50 +0800

    尝试实现动态ssl

--- src/main/kotlin/com/yrc/ddstreamclient/config/ssl/SslConfig.kt
+++ src/main/kotlin/com/yrc/ddstreamclient/config/ssl/SslConfig.kt
@@ -1,16 +1,46 @@
 package com.yrc.ddstreamclient.config.ssl
 
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
-import org.springframework.boot.web.server.WebServerFactoryCustomizer
+import nl.altindag.ssl.SSLFactory
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.io.FileInputStream
+import java.io.InputStream
+import javax.net.ssl.SSLSessionContext
+import javax.net.ssl.X509ExtendedKeyManager
+import javax.net.ssl.X509ExtendedTrustManager
 
 
-//@Configuration
+@Configuration
 class SslConfig {
     @Bean
-    fun tomcatSslStoreCustomizer(remoteSslStoreProvider: RemoteSslStoreProvider): WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
-        return WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
-            it.sslStoreProvider = remoteSslStoreProvider
-        }
+    fun keyManager(sslFactory: SSLFactory): X509ExtendedKeyManager {
+        return sslFactory.keyManager.orElseThrow()
     }
+
+    @Bean
+    fun trustManager(sslFactory: SSLFactory): X509ExtendedTrustManager {
+        return sslFactory.trustManager.orElseThrow()
+    }
+
+    @Bean
+    fun serverSessionContext(sslFactory: SSLFactory): SSLSessionContext {
+        return sslFactory.sslContext.serverSessionContext
+    }
+
+    @Bean
+    fun sslFactory(): SSLFactory {
+        return SSLFactory.builder()
+            .withIdentityMaterial(readStoreFromRemote(), "123456".toCharArray())
+            .withUnsafeTrustMaterial()
+            .build()
+    }
+
+    private fun readStoreFromRemote(): InputStream {
+        return FileInputStream("hls/key.keystore")
+    }
+//
+//    @Bean
+//    fun sslContextFactory(sslFactory: SSLFactory?): SslContextFactory.Server {
+//        return JettySslUtils.forServer(sslFactory)
+//    }
 }
